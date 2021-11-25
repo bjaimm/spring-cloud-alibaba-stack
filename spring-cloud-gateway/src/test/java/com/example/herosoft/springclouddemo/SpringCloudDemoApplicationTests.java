a9bbc231eee86bfeea59fb7911ab6dc47ead8491
bjaimm <bjaimm@hotmail.com>
2021-11-25 13:45:26 +0800

    Init commit

---
+++ spring-cloud-gateway/src/test/java/com/example/herosoft/springclouddemo/SpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herosoft.springclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
