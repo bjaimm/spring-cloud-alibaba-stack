216ab993ba421cf7bfd156e61d531221764c0e80
bjaimm <bjaimm@hotmail.com>
2021-12-03 17:45:11 +0800

    add RocketMQ message handling

--- spring-cloud-order-service/src/main/java/com/example/herosoft/springclouddemo/order/message/OrderTxMessageListener.java
+++ spring-cloud-order-service/src/main/java/com/example/herosoft/springclouddemo/order/message/OrderTxMessageListener.java
@@ -1,6 +1,17 @@
 package com.example.herosoft.springclouddemo.order.message;
 
 import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionState;
+import org.springframework.messaging.Message;
 
 public class OrderTxMessageListener implements RocketMQLocalTransactionListener {
+    @Override
+    public RocketMQLocalTransactionState executeLocalTransaction(Message message, Object o) {
+        return null;
+    }
+
+    @Override
+    public RocketMQLocalTransactionState checkLocalTransaction(Message message) {
+        return null;
+    }
 }
